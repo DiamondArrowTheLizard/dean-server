53daa76754327993adf98b2636b0f1dcab085199
DiamondArrowTheLizard <diamond1arrow@gmail.com>
2025-12-17 03:21:59 +0700

    configured DI

--- GUI/App.axaml.cs
+++ GUI/App.axaml.cs
@@ -6,6 +6,13 @@ using System.Linq;
 using Avalonia.Markup.Xaml;
 using GUI.ViewModels;
 using GUI.Views;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+using Microsoft.VisualBasic;
+using GUI.ViewModels.Authentication;
+using Interfaces.Handlers.Authentication;
+using Models.Handlers.Authentication;
 
 namespace GUI;
 
@@ -20,12 +27,16 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            _ = new ServiceCollection();
+            ServiceCollection? serviceCollection = ProvideServices();
+
+            var services = serviceCollection.BuildServiceProvider();
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = services.GetRequiredService<MainWindowViewModel>(),
             };
         }
 
@@ -43,5 +54,25 @@ public partial class App : Application
         {
             BindingPlugins.DataValidators.Remove(plugin);
         }
+    }
+
+    private static ServiceCollection ProvideServices()
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddSingleton<IAuthenticationHandler, AuthenticationHandler>();
+        collection.AddSingleton<IChangePassowrdHandler, ChangePassowrdHandler>();
+
+        collection.AddSingleton<MainWindowViewModel>();
+
+        collection.AddSingleton<AuthenticationViewModel>();
+        collection.AddSingleton<ChangePasswordViewModel>();
+        collection.AddSingleton<AuthenticationButtonViewModel>();
+        collection.AddSingleton<ChangePasswordButtonViewModel>();
+
+
+
+
+        return collection;
     }
 }
