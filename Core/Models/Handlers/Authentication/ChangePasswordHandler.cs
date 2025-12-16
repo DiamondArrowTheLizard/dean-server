e33fe3cb1b66c22abff43c7656f1ba798841639c
DiamondArrowTheLizard <diamond1arrow@gmail.com>
2025-12-17 04:54:12 +0700

    added debug message

--- Core/Models/Handlers/Authentication/ChangePasswordHandler.cs
+++ Core/Models/Handlers/Authentication/ChangePasswordHandler.cs
@@ -8,5 +8,6 @@ public class ChangePassowrdHandler : IChangePassowrdHandler
     public void HandlePasswordChange(string login, string oldPassword, string newPassword, string newPasswordConfirm)
     {
         // TODO
+        Console.WriteLine($"ChangePassword Contents:\nLogin: {login}\nOld Password:{oldPassword}\nNew Password:{newPassword}\nNew Password Confirm:{newPasswordConfirm}");
     }
 }
