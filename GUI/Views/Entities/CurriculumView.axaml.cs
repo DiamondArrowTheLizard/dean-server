2bd4c7f427da8e23c73def746bba4cc146d2a3c6
DiamondArrowTheLizard <diamond1arrow@gmail.com>
2025-12-29 01:05:11 +0700

    renamed ViewModel -> View

---
+++ GUI/Views/Entities/CurriculumView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GUI.Views.Entities;
+
+public partial class CurriculumView : UserControl
+{
+    public CurriculumView()
+    {
+        InitializeComponent();
+    }
+}
